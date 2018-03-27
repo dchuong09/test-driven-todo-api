9179bc7df0886890b6d1738d0d63615739553187
dchuong09 <dchuong09@yahoo.com>
2018-03-27 12:18:11 -0700

    completed to seach

--- server.js
+++ server.js
@@ -47,23 +47,42 @@ app.get('/api/todos/search', function search(req, res) {
   /* This endpoint responds with the search results from the
    * query in the request. COMPLETE THIS ENDPOINT LAST.
    */
+
 });
 
 app.get('/api/todos', function index(req, res) {
   /* This endpoint responds with all of the todos
    */
+   res.json({ data: todos });
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   // var todoId = req.body._id;
+   var todoTask = req.body.task;
+   var todoDesc = req.body.description;
+   var lastObj = todos[todos.length - 1];
+   var nextId = lastObj._id + 1
+   console.log(nextId);
+
+   var newObj = {_id: nextId, task: todoTask, description: todoDesc};
+
+   todos.push(newObj);
+   res.json(newObj);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
+   var todoId = req.params.id; 
+   var singleTodo = todos.filter(function(todo) {
+    // console.log(todo._id);
+    return todo._id == todoId;
+   })[0];
+   res.json(singleTodo);
 });
 
 app.put('/api/todos/:id', function update(req, res) {
@@ -71,6 +90,15 @@ app.put('/api/todos/:id', function update(req, res) {
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
+   var todoId = req.params.id;
+   todos = todos.forEach(function(todo){
+    if (todoId == todo._id) {
+      todo.task = req.body.task; 
+      todo.description = req.body.description;
+
+      res.json(todo);
+    }
+   });
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
@@ -78,6 +106,11 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with success.
    */
+   var todoId = req.params.id;
+   todos = todos.filter(function(todo){
+    return todoId != todo._id;
+   });
+   res.json(todos);
 });
 
 /**********
